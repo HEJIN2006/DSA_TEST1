c722397ac71dd7693bacefe2b8d3b33292cca4e8
HEJIN2006 <nguyenthithulan12121982@gmail.com>
2025-03-31 19:36:32 +0700

    Add files via upload

---
+++ cuoi/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace cuoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtNoiDung = new System.Windows.Forms.TextBox();
+            this.btnDang = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenNguoiDang = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblNhapten = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnQuayLai = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.pictureBox1.Image = global::cuoi.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // txtNoiDung
+            // 
+            this.txtNoiDung.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNoiDung.Location = new System.Drawing.Point(308, 82);
+            this.txtNoiDung.Multiline = true;
+            this.txtNoiDung.Name = "txtNoiDung";
+            this.txtNoiDung.Size = new System.Drawing.Size(175, 29);
+            this.txtNoiDung.TabIndex = 1;
+            this.txtNoiDung.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnDang
+            // 
+            this.btnDang.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDang.Location = new System.Drawing.Point(196, 132);
+            this.btnDang.Name = "btnDang";
+            this.btnDang.Size = new System.Drawing.Size(121, 50);
+            this.btnDang.TabIndex = 2;
+            this.btnDang.Text = "Đăng";
+            this.btnDang.UseVisualStyleBackColor = true;
+            this.btnDang.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(302, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bài viết gần đây";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-42, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(859, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = resources.GetString("label2.Text");
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtTenNguoiDang
+            // 
+            this.txtTenNguoiDang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenNguoiDang.Location = new System.Drawing.Point(308, 32);
+            this.txtTenNguoiDang.Name = "txtTenNguoiDang";
+            this.txtTenNguoiDang.Size = new System.Drawing.Size(175, 30);
+            this.txtTenNguoiDang.TabIndex = 7;
+            this.txtTenNguoiDang.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(105, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(240, 411);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblNhapten
+            // 
+            this.lblNhapten.AutoSize = true;
+            this.lblNhapten.BackColor = System.Drawing.Color.Yellow;
+            this.lblNhapten.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNhapten.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblNhapten.Location = new System.Drawing.Point(188, 33);
+            this.lblNhapten.Name = "lblNhapten";
+            this.lblNhapten.Size = new System.Drawing.Size(110, 29);
+            this.lblNhapten.TabIndex = 19;
+            this.lblNhapten.Text = "Nhập tên";
+            this.lblNhapten.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Yellow;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label12.Location = new System.Drawing.Point(188, 82);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(111, 29);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Nội dung";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.Color.Yellow;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label13.Location = new System.Drawing.Point(17, 28);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(45, 29);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "ID";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(81, 28);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(198, 29);
+            this.textBox3.TabIndex = 22;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.Location = new System.Drawing.Point(20, 77);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(140, 50);
+            this.btnTimKiem.TabIndex = 23;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(110, 330);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 16);
+            this.label14.TabIndex = 24;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.btnTimKiem);
+            this.panel1.Location = new System.Drawing.Point(489, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 155);
+            this.panel1.TabIndex = 28;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(166, 77);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(125, 50);
+            this.btnXoa.TabIndex = 24;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.White;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(12, 271);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(787, 207);
+            this.listBox1.TabIndex = 29;
+            // 
+            // btnQuayLai
+            // 
+            this.btnQuayLai.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnQuayLai.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuayLai.Location = new System.Drawing.Point(643, 411);
+            this.btnQuayLai.Name = "btnQuayLai";
+            this.btnQuayLai.Size = new System.Drawing.Size(140, 50);
+            this.btnQuayLai.TabIndex = 25;
+            this.btnQuayLai.Text = "Quay Lại";
+            this.btnQuayLai.UseVisualStyleBackColor = false;
+            this.btnQuayLai.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(330, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 50);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Chỉnh sửa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(810, 512);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnQuayLai);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lblNhapten);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTenNguoiDang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDang);
+            this.Controls.Add(this.txtNoiDung);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtNoiDung;
+        private System.Windows.Forms.Button btnDang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenNguoiDang;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblNhapten;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnQuayLai;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ cuoi/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cuoi
+{
+    public partial class Form1 : Form
+    {
+        // Định nghĩa Node cho LinkedList
+        class PostNode
+        {
+            public int ID; // ID bài viết
+            public string UserName; // Tên người đăng
+            public string Content; // Nội dung bài viết
+            public PostNode Next; // Trỏ đến bài viết tiếp theo
+
+            public PostNode(int id, string user, string content)
+            {
+                ID = id;
+                UserName = user;
+                Content = content;
+                Next = null;
+            }
+        }
+
+        private PostNode head = null; // Điểm bắt đầu của LinkedList
+        private int nextID = 1; // ID tự động tăng
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Không cần Console.ReadLine(), chỉ lấy nội dung từ TextBox
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Không cần Console.ReadLine(), chỉ lấy tên từ TextBox
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            // Không cần Console.ReadLine(), chỉ lấy ID từ TextBox
+            if (!string.IsNullOrWhiteSpace(textBox3.Text) && !int.TryParse(textBox3.Text, out _))
+            {
+                MessageBox.Show(" ID phải là một số!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox3.Clear(); // Xóa nội dung nhập sai
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userName = txtTenNguoiDang.Text;
+            string content = txtNoiDung.Text;
+
+            if (!string.IsNullOrWhiteSpace(userName) && !string.IsNullOrWhiteSpace(content))
+            {
+                // Tạo một bài viết mới trong LinkedList
+                PostNode newPost = new PostNode(nextID, userName, content);
+                nextID++; // Tăng ID cho bài đăng tiếp theo
+
+                if (head == null)
+                {
+                    head = newPost;
+                }
+                else
+                {
+                    PostNode temp = head;
+                    while (temp.Next != null)
+                    {
+                        temp = temp.Next;
+                    }
+                    temp.Next = newPost;
+                }
+
+                // Hiển thị lại danh sách bài viết
+                DisplayPosts();
+
+                // Xóa nội dung nhập vào
+                txtTenNguoiDang.Clear();
+                txtNoiDung.Clear();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox3.Text, out int id))
+            {
+                if (head == null) return;
+
+                if (head.ID == id)
+                {
+                    head = head.Next; // Xóa bài viết đầu tiên
+                }
+                else
+                {
+                    PostNode temp = head;
+                    while (temp.Next != null && temp.Next.ID != id)
+                    {
+                        temp = temp.Next;
+                    }
+                    if (temp.Next != null)
+                    {
+                        temp.Next = temp.Next.Next;
+                    }
+                }
+
+                DisplayPosts();
+                textBox3.Clear();
+            }
+        }
+
+        private void DisplayPosts()
+        {
+            listBox1.Items.Clear(); // Xóa danh sách cũ
+
+            PostNode temp = head;
+            while (temp != null)
+            {
+                listBox1.Items.Add($"ID: {temp.ID} | {temp.UserName}: {temp.Content}");
+                temp = temp.Next;
+            }
+        }
+
+        // Các sự kiện Label không cần xử lý gì
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void label6_Click(object sender, EventArgs e) { }
+        private void label8_Click(object sender, EventArgs e) { }
+        private void label11_Click(object sender, EventArgs e) { }
+        private void label13_Click(object sender, EventArgs e) { }
+        private void label15_Click(object sender, EventArgs e) { }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Khởi tạo danh sách bài viết nếu cần
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string searchText = textBox3.Text.Trim();
+            if (string.IsNullOrWhiteSpace(searchText)) return;
+
+            listBox1.Items.Clear(); // Xóa danh sách cũ trước khi hiển thị kết quả tìm kiếm
+            PostNode temp = head;
+            bool found = false;
+
+            while (temp != null)
+            {
+                if (int.TryParse(searchText, out int searchID) && temp.ID == searchID)
+                {
+                    // Tìm theo ID
+                    listBox1.Items.Add($"ID: {temp.ID} | {temp.UserName}: {temp.Content}");
+                    found = true;
+                    break; // Vì ID là duy nhất, không cần tìm tiếp
+                }
+                else if (temp.UserName.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    // Tìm theo tên người đăng (không phân biệt hoa thường)
+                    listBox1.Items.Add($"ID: {temp.ID} | {temp.UserName}: {temp.Content}");
+                    found = true;
+                }
+                temp = temp.Next;
+            }
+
+            if (!found)
+            {
+                listBox1.Items.Add("❌ Không tìm thấy bài viết!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DisplayPosts(); // Gọi lại hàm hiển thị danh sách đầy đủ
+            textBox3.Clear();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox3.Text, out int postId))
+            {
+                MessageBox.Show("⚠️ ID phải là một số!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string newUserName = txtTenNguoiDang.Text.Trim();
+            string newContent = txtNoiDung.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(newUserName) || string.IsNullOrWhiteSpace(newContent))
+            {
+                MessageBox.Show("⚠️ Vui lòng nhập đầy đủ tên người đăng và nội dung!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            PostNode temp = head;
+            bool found = false;
+
+            while (temp != null)
+            {
+                if (temp.ID == postId)
+                {
+                    temp.UserName = newUserName;
+                    temp.Content = newContent;
+                    found = true;
+                    break;
+                }
+                temp = temp.Next;
+            }
+
+            if (found)
+            {
+                MessageBox.Show("✅ Bài đăng đã được cập nhật!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DisplayPosts(); // Cập nhật danh sách hiển thị
+                textBox3.Clear();
+                txtTenNguoiDang.Clear();
+                txtNoiDung.Clear();
+            }
+            else
+            {
+                MessageBox.Show("❌ Không tìm thấy bài đăng có ID này!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
